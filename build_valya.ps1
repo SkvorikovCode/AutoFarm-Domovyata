f418238b41361638cdc84acfd3f1ef9bc2c9a531
Denis <167305961+SkvorikovCode@users.noreply.github.com>
2025-06-24 17:32:19 +0300

    Update build_valya.ps1

--- build_valya.ps1
+++ build_valya.ps1
@@ -11,6 +11,6 @@ pip install -r requirements.txt
 pip install pyinstaller
 
 # Сборка main.py в exe
-pyinstaller --onefile --noconsole main.py
+pyinstaller --onefile main.py
 
 Write-Host "\nГотово! Файл main.exe находится в папке dist." 
