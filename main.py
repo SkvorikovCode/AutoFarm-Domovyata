2a74dd07f8ef6bd61399ff932d068e79386c2b23
Denis <skvorikoffg@gmail.com>
2025-06-24 17:47:36 +0300

    Increase wait times and click delays in main.py
    Adjusted various wait and delay values throughout the main() function to use longer durations. This likely improves timing reliability and stability for automated actions.

--- main.py
+++ main.py
@@ -79,69 +79,69 @@ def main():
     global stop_script, script_running
     # --- Посадка низ ---
     lclick(50, 404)
-    waitms(20)
+    waitms(50)
 
     ldown(290, 276)
-    wait(0.2)
+    wait(0.5)
     lup(290, 276)
-    wait(0.2)
+    wait(0.5)
 
     move(1142, 612)
-    waitms(40)
+    waitms(100)
     ldown(745, 405)
-    waitms(40)
+    waitms(100)
     lup(745, 405)
-    waitms(80)
+    waitms(200)
 
     for var in range(175, 1850, 2):
         if stop_script: return
-        lclick(var + rnd(-2, 0), 276 + rnd(-3, 1), 20)
+        lclick(var + rnd(-2, 0), 276 + rnd(-3, 1), 45)
 
     lclick(1850, 350)
-    wait(1.5)
+    wait(4)
 
     # --- Посадка верх ---
     ldown(580, 132)
-    wait(0.2)
+    wait(0.5)
     lup(580, 132)
-    wait(0.2)
+    wait(0.5)
 
     move(1142, 612)
-    waitms(40)
+    waitms(100)
     ldown(745, 405)
-    waitms(40)
+    waitms(100)
     lup(745, 405)
-    waitms(80)
+    waitms(200)
 
     for var in range(517, 1410, 2):
         if stop_script: return
-        lclick(var + rnd(-2, 0), 132 + rnd(-3, 1), 20)
+        lclick(var + rnd(-2, 0), 132 + rnd(-3, 1), 45)
 
     lclick(1850, 350)
-    wait(1.5)
+    wait(4)
 
     # --- Сбор низ до крестика ---
     for var in range(150, 1145, 4):
         if stop_script: return
         for offset in range(0, 7, 2):
-            lclick(var + offset + rnd(-2, 1), 155 + rnd(-1, 1), 15)
-        lclick(1265 + rnd(-3, 3), 160 + rnd(-3, 3), 10)
+            lclick(var + offset + rnd(-2, 1), 155 + rnd(-1, 1), 33)
+        lclick(1265 + rnd(-3, 3), 160 + rnd(-3, 3), 22)
 
     # --- Сбор низ после крестика ---
     for var in range(1260, 1825, 4):
         if stop_script: return
         for offset in range(0, 7, 2):
-            lclick(var + offset + rnd(-2, 1), 155 + rnd(-1, 1), 15)
-        lclick(1265 + rnd(-3, 3), 160 + rnd(-3, 3), 10)
+            lclick(var + offset + rnd(-2, 1), 155 + rnd(-1, 1), 33)
+        lclick(1265 + rnd(-3, 3), 160 + rnd(-3, 3), 22)
 
     # --- Сбор верх ---
     for var in range(480, 1410, 3):
         if stop_script: return
         for offset in range(0, 7, 2):
-            lclick(var + offset + rnd(-2, 1), 49 + rnd(-2, 2), 10)
-        lclick(1265 + rnd(-3, 3), 160 + rnd(-3, 3), 7)
+            lclick(var + offset + rnd(-2, 1), 49 + rnd(-2, 2), 20)
+        lclick(1265 + rnd(-3, 3), 160 + rnd(-3, 3), 16)
 
-    wait(rnd(-0.5, 0.5))
+    wait(rnd(-1.5, 1.5))
     script_running = False
 
 if __name__ == "__main__":
